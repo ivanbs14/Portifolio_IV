20e5a07033b1f690b29b1b2ad7b3efbbdcfdbc66
Ivan Barbosa <ivan.barbosa@bugaboostudio.com>
2024-05-27 09:26:57 -0300

    refact: scrollbar

--- src/styles/global.js
+++ src/styles/global.js
@@ -18,6 +18,41 @@ export default createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
+    /* Personaliza toda a barra de rolagem */
+    ::-webkit-scrollbar {
+        width: 0px;  /* Define a largura da barra de rolagem vertical como 0 */
+        height: 0px; /* Define a altura da barra de rolagem horizontal como 0 */
+    }
+
+    /* Personaliza os botões da barra de rolagem (setas) */
+    ::-webkit-scrollbar-button {
+        display: none; /* Oculta os botões da barra de rolagem */
+    }
+
+    /* Personaliza a alça de rolagem (arrastável) */
+    ::-webkit-scrollbar-thumb {
+        background-color: transparent; /* Torna a alça de rolagem invisível */
+    }
+
+    /* Personaliza a faixa da barra de rolagem (barra de progresso) */
+    ::-webkit-scrollbar-track {
+        background-color: transparent; /* Torna a faixa da barra de rolagem invisível */
+    }
+
+    /* Personaliza parte do caminho da barra de progresso */
+    ::-webkit-scrollbar-track-piece {
+        background-color: transparent; /* Torna parte do caminho da barra de rolagem invisível */
+    }
+
+    /* Personaliza o canto inferior da barra de rolagem */
+    ::-webkit-scrollbar-corner {
+        background-color: transparent; /* Torna o canto inferior invisível */
+    }
+
+    /* Personaliza a alça arrastável no canto inferior */
+    ::-webkit-resizer {
+        display: none; /* Oculta a alça arrastável no canto inferior */
+    }
 
     body, input, button, textarea {
         font-size: 1.6rem;
