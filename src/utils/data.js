30e1596fcc8c5b0aae990b26bd6e5d5085335838
Ivan Barbosa <ivan.barbosa@bugaboostudio.com>
2024-05-28 15:10:56 -0300

    feat: add new projects

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,36 +1,45 @@
+import imgIA from '../assets/01.png';
 import imgIfood from '../assets/foodexplorer.png';
+import imgMemory from '../assets/memory.png';
 import imgMovie from '../assets/notmovie.png';
 import imgNotpad from '../assets/notpad.png';
 import imgTypeW from '../assets/typeW.png';
-import imgIA from '../assets/01.png';
 
 export const projectsData = [
    {
      imgLogo: imgIA,
-     titleCd: "FOOD EXPLORER",
+     titleCd: "AI copilador de Video...",
      subTitleCd: "Desenvolvido uma aplicação web, que recebe um video e interagindo com a API do chatgpt gera títulos, hashtags, e um resumo, do conteúdo do video.",
      btnRefGit: "https://github.com/ivanbs14/Upload_AI_web.git",
-     btnRefTwo: "https://www.linkedin.com/feed/update/urn:li:activity:7115362398164688896/",
      icons: { nd: true, ts: true ,js: true, rc: true, tw: true, },
-     video: 'https://www.youtube.com/watch?v=iu-LBY7NXD4',
+     video: 'https://youtu.be/hX-rhRG8sfQ',
    },
    {
      imgLogo: imgIfood,
      titleCd: "FOOD EXPLORER",
      subTitleCd: "API RESTFULL. O food explorer, é um menu interativo para um restaurante...",
      btnRefGit: "https://github.com/ivanbs14/FoodExplore_Front.git",
-     btnRef: "https://foofexploreriv.netlify.app/",
+     //btnRef: "https://foofexploreriv.netlify.app/",
      icons: { nd: true, js: true, sc: true, rc: true },
-     video: 'https://www.youtube.com/watch?v=iu-LBY7NXD4',
+     video: 'https://youtu.be/kRMU0gorPB8',
+   },
+   {
+     imgLogo: imgMemory,
+     titleCd: "Jogo da memória",
+     subTitleCd: "Um jogo da memória é um desafio mental onde os jogadores revelam cartas emparelhadas, tentando lembrar suas posições. O objetivo é encontrar todos os pares correspondentes, melhorando a memória e a concentração.",
+     btnRefGit: "https://github.com/ivanbs14/App_memory.git",
+     //btnRef: "https://foofexploreriv.netlify.app/",
+     icons: { nd: true, js: true, sc: true, rc: true,ts: true },
+     video: 'https://youtu.be/Svwc3Kv0QYE',
    },
    {
      imgLogo: imgMovie,
      titleCd: "ROCKETMOVIES",
-     subTitleCd: "API RESTFULL. Onde após criar um cadastro e fazer Login. O usuário vai cadastrar o filme que assistiu...",
+     subTitleCd: "API RESTFULL. Onde após criar um cadastro e fazer Login. O usuário vai cadastrar seus filmes...",
      btnRefGit: "https://github.com/ivanbs14/RocketMOvies_Front-End.git",
-     btnRef: "https://notemovies.netlify.app/",
+     //btnRef: "https://notemovies.netlify.app/",
      icons: { nd: true, js: true, sc: true, rc: true },
-     video: 'https://www.youtube.com/watch?v=iu-LBY7NXD4',
+     video: 'https://youtu.be/lZKgm5_hVY8',
    },
    {
      imgLogo: imgTypeW,
@@ -38,14 +47,14 @@ export const projectsData = [
      subTitleCd: "Descubra o clima com facilidade! Esta aplicação fornece informações precisas e atualizadas sobre o clima em qualquer lugar do mundo...",
      btnRefGit: "https://github.com/ivanbs14/App_Clima.git",
      icons: { ts: true, nd: true, js: true, sc: true, rc: true },
-     video: 'https://www.youtube.com/watch?v=iu-LBY7NXD4',
+     video: 'https://youtu.be/LFhiYoKkfRk',
    },
    {
      imgLogo: imgNotpad,
      titleCd: "NOTPAD",
      subTitleCd: "API RESTFULL. Onde após criar um cadastro e fazer Login. O usuário vai cadastrar o filme que assistiu...",
      btnRefGit: "https://github.com/ivanbs14/NotPad_Front.git",
-     btnRef: "https://notepadss.netlify.app/",
+     //btnRef: "https://notepadss.netlify.app/",
      icons: { nd: true, js: true, sc: true, rc: true },
      video: 'https://www.youtube.com/watch?v=iu-LBY7NXD4',
    }
